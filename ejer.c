dc70ea36bcbc988060b9d213cfdc0b9d7c2d162a
Thais Rojas <thais.rojas@udla.edu.ec>
2023-11-21 15:10:39 -0500

    crea la matriz

---
+++ ejer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int filas, columnas;
+
+    // Solicita al usuario las dimensiones 
+    printf("Ingrese el número de filas de la matriz: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas de la matriz: ");
+    scanf("%d", &columnas);
+
+    // Crea la matriz con las dimensiones dadas
+    int matriz[filas][columnas];
+
+}
